172d851f4f60ca57f02afa11d58c33637a5ae75d
Khalid <mustkre8@gmail.com>
2021-08-27 10:12:33 -0700

    .

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import { S3Client, GetObjectCommand, GetObjectCommandInput } from "@aws-sdk/client-s3"
 
 export async function readValue(key: string, bucket: string = "my-bucket") {
+    console.log(`reading value for ${key} in ${bucket}`)
     const client = new S3Client({ region: "us-west-2" });
     const params: GetObjectCommandInput = {
         Bucket: bucket, Key: key
@@ -10,9 +11,11 @@ export async function readValue(key: string, bucket: string = "my-bucket") {
     try {
         const data = await client.send(command)
         // process data.
+        console.log("successfully fetched data")
         console.log(data)
     } catch (error) {
         // error handling.
+        console.log("failed to fetch data")
         console.error(error)
     } finally {
         // finally.
